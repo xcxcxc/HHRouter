6e467090fa8a7c348a9072bad7850a7d511bc478
xc <xc20675942@qq.com>
2014-08-27 17:02:56 +0800

    Revert "修复 URI解释后面的参数的bug"

--- HHRouter/HHRouter.m
+++ HHRouter/HHRouter.m
@@ -167,11 +167,12 @@
 - (NSArray*)pathComponentsFromRoute:(NSString*)route
 {
     NSMutableArray *pathComponents = [NSMutableArray array];
-    for (NSString *pathComponent in [[NSURL URLWithString:route] pathComponents]) {
+    for (NSString *pathComponent in route.pathComponents) {
         if ([pathComponent isEqualToString:@"/"]) continue;
         if ([[pathComponent substringToIndex:1] isEqualToString:@"?"]) break;
         [pathComponents addObject:pathComponent];
     }
+
     return [pathComponents copy];
 }
 
